314ade5faa1d6ba4d0a3f216a76ae7a5a042f556
Your Name <maonoreakvichea@gmail.com>
2025-10-06 13:10:22 +0700

    hw

--- test.cpp
+++ test.cpp
@@ -1,18 +1,39 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
- int main(){
-    int a[5] = {5,4,3,2,1};
-    int n = 5;
-    a[2] = 11;
-
-    for(int i = 1; i < n-1 ; i++){
-        a[i] = a[i+1];
+void insertElement(int *arr, int pos, int num, int &n){
+    if (pos > n || pos < 0){
+        cout<<"error"<<endl;
+        return;
+    }
+    for(int i = n; i > pos; i--){
+        arr[i] = arr[i - 1];
+    }
+    arr[pos] = num;
+    n++;
+}
+void deleteElement(int *arr, int pos, int &n){
+    if (pos > n || pos < 0){
+        cout<<"error"<<endl;
+        return;
+    }
+    for (int i = pos; i < n - 1; i++){
+        arr[i] = arr[i + 1];
     }
     n--;
-    for(int i = 0 ; i < n ; i++){
-        cout <<a[i]<< " ";
-   }
+}
 
-    return 0;
- }
+int main(){
+    int arr[] = {5,4,3,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+insertElement(arr, 2, 19, n);
+cout<<"after insert element ="<<endl;
+for(int i = 0; i < n; i++){
+    cout<<arr[i]<<" ";
+}
+deleteElement(arr, 0, n);
+cout<<"after delete element"<<endl;
+for(int i = 0; i < n; i++){
+    cout<<arr[i]<<" ";
+}
+return 0;
+}
